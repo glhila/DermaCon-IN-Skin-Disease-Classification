c7ad864ec7d939cbe8bd5ce9261f900ce1d03381
glhila <hilaglazz13@gmail.com>
2025-08-26 15:44:36 +0300

    Refactor run_models function to use match-case structure for improved readability and error handling. Added a case for invalid modes to raise a ValueError.

--- main.py
+++ main.py
@@ -16,17 +16,21 @@ from train_model_NQ import train_model
 from newTester import evaluate_saved_model
 
 def run_models(mode):
-    if mode == 'A':
-        print("Running Model A: Non-quantized data + Non-quantized model")
-        train_loader, val_loader, test_loader, _ = prepare_data(batch_size=32, num_workers=0, quantize_input= False)
-        train_model(data_is_quantized=False, stage_epochs=(6,20,24), early_stop_patience=6, train_loader=train_loader, val_loader=val_loader)
-        evaluate_saved_model("mobilenetv2_best_not_quantized.pth", mode="fp32", test_loader=test_loader)
+    match mode:
+        case 'A':
+            print("Running Model A: Non-quantized data + Non-quantized model")
+            train_loader, val_loader, test_loader, _ = prepare_data(batch_size=32, num_workers=0, quantize_input=False)
+            train_model(data_is_quantized=False, stage_epochs=(6,20,24), early_stop_patience=6, train_loader=train_loader, val_loader=val_loader)
+            evaluate_saved_model("mobilenetv2_best_not_quantized.pth", mode="fp32", test_loader=test_loader)
 
-    elif mode == 'B':
-        print("Running Model B: Quantized data + Non-quantized model")
-        train_loader, val_loader, test_loader, _ = prepare_data(batch_size=32, num_workers=0, quantize_input= True)
-        train_model(data_is_quantized=True, stage_epochs=(6,20,24), early_stop_patience=6, train_loader=train_loader, val_loader=val_loader)
-        evaluate_saved_model("mobilenetv2_best_data_quantized.pth", mode="fp32", test_loader=test_loader)
+        case 'B':
+            print("Running Model B: Quantized data + Non-quantized model")
+            train_loader, val_loader, test_loader, _ = prepare_data(batch_size=32, num_workers=0, quantize_input=True)
+            train_model(data_is_quantized=True, stage_epochs=(6,20,24), early_stop_patience=6, train_loader=train_loader, val_loader=val_loader)
+            evaluate_saved_model("mobilenetv2_best_data_quantized.pth", mode="fp32", test_loader=test_loader)
+
+        case _:
+            raise ValueError("Invalid mode. Choose 'A' or 'B' or 'C' or 'D'.")
 
     #elif mode == 'C':
     #    print("Running Model C: Non-quantized data + Quantized model")
